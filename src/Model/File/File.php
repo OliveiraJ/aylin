3294f33ed2ab20387dd55da4b44203c9cd20b379
Jordan <jordansilva102@gmail.com>
2026-05-12 21:16:09 -0300

    feat: basic poc model, defining basic structures of file and tag models

---
+++ src/Model/File/File.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Oliveiraj\Aylin\Model;
+
+use DateTimeInterface;
+use DateTimeImmutable;
+use DomainException;
+use Oliveiraj\Aylin\Model\Tag;
+
+class File
+{
+    private ?int $id;
+    private string $path;
+    /** @var Tag[] $tags */
+    private array $tags;
+    private DateTimeInterface $createdAt;
+    private ?DateTimeInterface $updatedAt;
+
+    /**
+     * @param string $path;
+     * @param Tag[] $tags;
+     */
+    public function __construct(?int $id, string $path, array $tags = [])
+    {
+        $this->id = $id;
+        $this->path = $path;
+        $this->tags = $tags;
+        $this->createdAt = new DateTimeImmutable();
+        $this->updatedAt = null;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+
+    public function setPath(string $path): void
+    {
+        $this->path = $path;
+    }
+
+    public function getCreatedAt(): DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function getUpdatedAt(): DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedDate(DateTimeInterface $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    public function setTag(Tag $tag): void
+    {
+        $this->tags[] = $tag;
+    }
+
+    public function removeTag(int $id): void
+    {
+        $exists = array_any($this->tags, fn(Tag $tag) => $tag->getId() === $id);
+
+        if (!$exists) {
+            throw new DomainException("Tag {$id} not found.");
+        }
+
+        $this->tags = array_values(
+            array_filter($this->tags, fn(Tag $tag) => $tag->getId() !== $id),
+        );
+    }
+
+    public function getTags(): array
+    {
+        return $this->tags;
+    }
+}
---
+++ src/Model/Tag/Tag.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Oliveiraj\Aylin\Model;
+
+use DateTimeImmutable;
+use DateTimeInterface;
+
+class Tag
+{
+    private ?int $id;
+    private string $name;
+    private DateTimeInterface $createdAt;
+    private ?DateTimeInterface $updatedAt;
+
+    public function __construct(?int $id, string $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->createdAt = new DateTimeImmutable();
+        $this->updatedAt = null;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setName(string $name)
+    {
+        return $this->name = $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getCreatedAt(): DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function getUpdatedAt(): DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+}
